aa8f9e4337fdd5df92f288a1cdcd9fcafc7c5417
unknown <assimataya@gmail.com>
2022-03-15 12:20:37 +0000

    adding modules and npm

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// new and easy why of importing modules in Javascript 
+//import { without } from "lodash";
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6];
+console.log('answer', _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
